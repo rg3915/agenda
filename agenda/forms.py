9662f3e699af4a44b1378a2ffdef4be7541179e7
rg3915 <rg3915@yahoo.com.br>
2018-10-01 22:15:14 -0300

    widget dentro de Class Meta

--- agenda/forms.py
+++ agenda/forms.py
@@ -3,28 +3,36 @@ from .models import Cadastro
 
 
 class CadastroForm(forms.ModelForm):
-    widgets = {
-            'nome': forms.TextInput(attrs={
-                'class': 'form-control col-sm-12 form-control-lg',
-                'type': 'text', }),
-            'email': forms.TextInput(attrs={
-                'class': 'form-control col-sm-12 form-control-lg',
-                'type': 'text', }),
-            'endereco': forms.TextInput(attrs={
-                'class': 'form-control col-sm-12 form-control-lg',
-                'type': 'text', }),
-            'sexo': forms.Select(attrs={
-                'class': 'custom-select form-control-lg', }),
-            'telefone': forms.TextInput(attrs={
-                'class': 'form-control col-sm-12 form-control-lg',
-                'type': 'text', }),
-            'celular': forms.TextInput(attrs={
-                'class': 'form-control col-sm-12 form-control-lg',
-                'type': 'text', }),
-            'cargo': forms.TextInput(attrs={
-                'class': 'form-control col-sm-12 form-control-lg',
-                'type': 'text', }),
-        }
+
     class Meta:
         model = Cadastro
         fields = '__all__'
+        widgets = {
+            'nome': forms.TextInput(attrs={
+                'class': 'form-control',
+                'type': 'text',
+            }),
+            'email': forms.TextInput(attrs={
+                'class': 'form-control',
+                'type': 'text',
+            }),
+            'endereco': forms.TextInput(attrs={
+                'class': 'form-control',
+                'type': 'text',
+            }),
+            'sexo': forms.Select(attrs={
+                'class': 'custom-select form-control', }
+            ),
+            'telefone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'type': 'text',
+            }),
+            'celular': forms.TextInput(attrs={
+                'class': 'form-control',
+                'type': 'text',
+            }),
+            'cargo': forms.TextInput(attrs={
+                'class': 'form-control',
+                'type': 'text',
+            }),
+        }
