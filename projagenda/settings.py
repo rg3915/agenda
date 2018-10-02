88ffc56b1bc40f3066ea144251b241b15750cc2b
rg3915 <rg3915@yahoo.com.br>
2018-10-01 21:59:12 -0300

    mexi um pouquinho no seu settings

--- projagenda/settings.py
+++ projagenda/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config, Csv
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,15 +77,20 @@ WSGI_APPLICATION = 'projagenda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'agenda',
+#         'USER': 'postgres',
+#         'PASSWORD': 'wilci5w7',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'agenda',
-        'USER': 'postgres',
-        'PASSWORD': 'wilci5w7',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 
